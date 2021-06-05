bfd0b5e7003e4d2f3db6c0d8d1f93749425f9602
dargor980 <german.contrerasa@utem.cl>
2021-06-05 16:33:54 -0400

    Final Commit

---
+++ README-G4.md
@@ -0,0 +1,11 @@
+Asignatura: Redes y comunicación de datos.
+Trabajo 1
+Grupo 4
+link: https://github.com/dargor980/RCD-Trabajo-1.git
+Integrantes:
+Luciano Grandi Morales
+Germán Contreras Améstica
+Benjamín Cristi Simonsen
+Manuel Toledo Levicán 
+Oscar Muñoz Retamal
+Matías Inostroza Quinteros
---
+++ REST/Rest/app/Http/Controllers/GlobalController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+
+class GlobalController extends Controller
+{
+    public function rut(Request $request){
+        try {
+            Log::info("Inicia Funcion Rut");
+            $gets = $request->json()->all();
+            $rut = $gets["rut"];
+            $rut = str_replace(".","",$rut);
+            if(strlen($rut) < 7 ){
+                Log::error("Rut inválido");
+                return 9999;
+            }else{
+                $arr=strrev($rut);
+                $arr= str_split($arr);
+                $serie=2;
+                $suma=0;
+                $c=0;
+                while($c<8){
+                    if($serie<=7){
+                        $suma+=$arr[$c]*$serie;
+                        $serie++;
+                    }
+                    else{
+                        $serie=2;
+                        $suma+=$arr[$c]*$serie;
+                        $serie++;
+                    }
+                    $c++;
+                }
+                $suma %=11;
+                $suma=11- $suma;
+                if($suma==10){
+                    $k = 10;
+                    return $k;
+                }
+                else{
+                    if($suma == 11){
+                        // 0
+                        return 11;
+                    }
+                    else{
+                        if($suma<10){
+                            return $suma;
+                        }
+                        else{
+                            Log::error("Rut inválido");
+                            return 9999;
+                        }
+                    }
+                }
+            }
+        } catch (\Exception $exc) {
+            Log::error($exc->getMessage());    
+        }
+        
+    }
+    public function nombre(Request $request){
+        try {
+            Log::info("Inicia funcion nombre");
+            $gets = $request->json()->all();
+            $nombre = $gets["fullName"];
+            $nombre_explode = explode(" ", $nombre);
+            $count_nombres = count($nombre_explode);
+            $apellidos = array();
+            array_push($apellidos,$nombre_explode[$count_nombres-2]);
+            array_push($apellidos,$nombre_explode[$count_nombres-1]);
+            $nombres = array();
+            for ($i=0; $i < $count_nombres-2 ; $i++) { 
+                array_push($nombres,$nombre_explode[$i]);
+            }
+            $final = array();
+            array_push($final,$nombres);
+            array_push($final,$apellidos);
+            return $final;
+        } catch (\Exception $exc) {
+            Log::error($exc->getMessage());
+            return "Error";   
+        }
+        
+    }
+}
